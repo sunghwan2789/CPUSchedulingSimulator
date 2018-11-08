636f450a33166f4f60ed776f10fdb3f8b7c6ce7a
Sunghwan Bang <sunghwan2789@gmail.com>
2018-11-08 17:57:10 +0900

    라운드 로빈 시간 할당량 필드 추가

--- SchedulerSimulator/RoundRobin.cs
+++ SchedulerSimulator/RoundRobin.cs
@@ -8,6 +8,11 @@ namespace SchedulerSimulator
 {
     class RoundRobin : Scheduler
     {
+        /// <summary>
+        /// 시간 할당량
+        /// </summary>
+        public int TimeQuantum { get; set; }
+
         private readonly Queue<ProcessControlBlock> readyQueue = new Queue<ProcessControlBlock>();
 
         public override void Push(Process process) => throw new NotImplementedException();
