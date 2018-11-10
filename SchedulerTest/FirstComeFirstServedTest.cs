b255b3fd575049014b6b9a4881a623a27870c843
Sunghwan Bang <sunghwan2789@gmail.com>
2018-11-10 17:27:38 +0900

    테스트 데이터 작성

---
+++ SchedulerTest/FirstComeFirstServedTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SchedulerSimulator;
+
+namespace SchedulerTest
+{
+    [TestClass]
+    public class FirstComeFirstServedTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var s = new FirstComeFirstServed();
+            var expected = new[]
+            {
+                new ProcessControlBlock
+                {
+                    Process = new Process
+                    {
+                        ProcessId = "P0",
+                    },
+                    ResponseTime = 0,
+                    WaitingTime = 0,
+                    BurstTime = 7,
+                    TurnaroundTime = 7 - 0,
+                },
+                new ProcessControlBlock
+                {
+                    Process = new Process
+                    {
+                        ProcessId = "P1",
+                    },
+                    ResponseTime = 7 - 2,
+                    WaitingTime = 7 - 2,
+                    BurstTime = 4,
+                    TurnaroundTime = 7 - 2 + 4,
+                },
+                new ProcessControlBlock
+                {
+                    Process = new Process
+                    {
+                        ProcessId = "P2",
+                    },
+                    ResponseTime = (7 - 2 + 4) - 3,
+                    WaitingTime = (7 - 2 + 4) - 3,
+                    BurstTime = 1,
+                    TurnaroundTime = (7 - 2 + 4) - 3 + 1,
+                },
+                new ProcessControlBlock
+                {
+                    Process = new Process
+                    {
+                        ProcessId = "P3",
+                    },
+                    ResponseTime = ((7 - 2 + 4) - 3 + 1) - 6,
+                    WaitingTime = ((7 - 2 + 4) - 3 + 1) - 6,
+                    BurstTime = 3,
+                    TurnaroundTime = ((7 - 2 + 4) - 3 + 1) - 6 + 3,
+                },
+            };
+            var seq = 0;
+            s.ProcessChanged = (pcb) =>
+            {
+                var e = expected[seq++];
+                Assert.AreEqual(e.Process.ProcessId, pcb.Process.ProcessId, "프로세스 ID 불일치");
+                Assert.AreEqual(e.WaitingTime, pcb.WaitingTime, "대기 시간 불일치");
+                Assert.AreEqual(e.BurstTime, pcb.BurstTime, "서비스 시간 불일치");
+                Assert.AreEqual(e.ResponseTime, pcb.ResponseTime, "응답 시간 불일치");
+                Assert.AreEqual(e.TurnaroundTime, pcb.TurnaroundTime, "반환 시간 불일치");
+            };
+            foreach (var p in Testdata.Processes)
+            {
+                s.Push(p);
+            }
+        }
+    }
+}
---
+++ SchedulerTest/Testdata.cs
@@ -0,0 +1,35 @@
+﻿using SchedulerSimulator;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulerTest
+{
+    class Testdata
+    {
+        // PID, 도착 시간, 실행 시간, 우선순위
+        public const string ex01 = @"4
+P0 0 7 3
+P1 2 4 2
+P2 3 1 4
+P3 6 3 1
+2";
+        public static Process[] Processes;
+        public static int TimeQuantum;
+
+        static Testdata()
+        {
+            var lines = ex01.Split('\n');
+            var count = int.Parse(lines[0]);
+            Processes = new Process[count];
+            for (var i = 0; i < count; i++)
+            {
+                Processes[i] = Process.Parse(lines[i + 1]);
+            }
+            TimeQuantum = int.Parse(lines[count + 1]);
+        }
+    }
+}
