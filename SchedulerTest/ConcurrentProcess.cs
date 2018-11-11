cab2e40e1d9f74da18ba9ef2020ab2f94337fd19
Sunghwan Bang <sunghwan2789@gmail.com>
2018-11-11 15:10:08 +0900

    도착 시간이 같은 프로세스가 여러 개 있을 때를 테스트하도록 추가

---
+++ SchedulerTest/ConcurrentProcess.cs
@@ -0,0 +1,34 @@
+﻿using SchedulerSimulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulerTest
+{
+    class ConcurrentProcess
+    {
+        // PID, 도착 시간, 실행 시간, 우선순위
+        public const string ex01 = @"4
+P0 0 7 3
+P1 0 3 2
+P2 2 1 4
+P3 4 7 1
+2";
+        public static Process[] Processes;
+        public static int TimeQuantum;
+
+        static ConcurrentProcess()
+        {
+            var lines = ex01.Split('\n');
+            var count = int.Parse(lines[0]);
+            Processes = new Process[count];
+            for (var i = 0; i < count; i++)
+            {
+                Processes[i] = Process.Parse(lines[i + 1]);
+            }
+            TimeQuantum = int.Parse(lines[count + 1]);
+        }
+    }
+}
