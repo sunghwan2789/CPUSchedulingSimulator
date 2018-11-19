543ee0dce62cc77521d851a0e7f6b98034ce07a7
Hwang JeongHwan <5832120@naver.com>
2018-11-19 17:06:14 +0900

    HighResponseRatioNext추가, Priority 타입 변경

--- SchedulerSimulator/HighestResponseRatioNext.cs
+++ SchedulerSimulator/HighestResponseRatioNext.cs
@@ -10,9 +10,72 @@ namespace SchedulerSimulator
     {
         private readonly List<ProcessControlBlock> readyQueue = new List<ProcessControlBlock>();
 
-        protected override bool Busy => throw new NotImplementedException();
+        protected override bool Busy => readyQueue.Any();
 
-        public override void Push(Process process) => throw new NotImplementedException();
-        protected override void Dispatch() => throw new NotImplementedException();
+        public override void Push(Process process)
+        {
+            if (
+               readyQueue.Any()
+               && process.ArrivalTime > readyQueue.First().Process.ArrivalTime
+             )
+            {
+                while (readyQueue.Any() && process.ArrivalTime > currentTime)
+                {
+                    Dispatch();
+                }
+            }
+            readyQueue.Add(new ProcessControlBlock
+            {
+                Process = process,
+            });
+        }
+        protected override void Dispatch()
+        {
+            foreach (var rpcb in readyQueue)
+            {
+                rpcb.Process.Priority = (double)currentTime / rpcb.Process.BurstTime;
+            }
+            readyQueue.Sort(new PriorityCompare());
+
+            var before = readyQueue.First();
+            readyQueue.RemoveAt(0);
+            var pcb = (ProcessControlBlock)before.Clone();
+            var process = pcb.Process;
+
+            // 프로세스 도착 시간까지 현재 시간을 진행
+            if (currentTime < process.ArrivalTime)
+            {
+                currentTime = process.ArrivalTime;
+            }
+
+            pcb.ResponseTime = currentTime - process.ArrivalTime;
+            pcb.WaitingTime = currentTime - process.ArrivalTime;
+
+            currentTime += process.BurstTime;
+            pcb.BurstTime = process.BurstTime;
+
+            pcb.TurnaroundTime = currentTime - process.ArrivalTime;
+
+            OnProcessChanged(pcb);
+        }
+
+        private class PriorityCompare : Comparer<ProcessControlBlock>
+        {
+            public override int Compare(ProcessControlBlock x, ProcessControlBlock y)
+            {
+                if (x.Process.Priority == y.Process.Priority)
+                {
+                    return 0;
+                }
+                else if (x.Process.Priority < y.Process.Priority)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
     }
 }
