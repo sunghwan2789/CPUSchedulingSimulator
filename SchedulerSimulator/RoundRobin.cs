e1d02822deca24a78d5439df58d840e9b148ab50
Sunghwan Bang <sunghwan2789@gmail.com>
2018-11-21 19:19:34 +0900

    RR 기본 코드 컨셉 작성

--- SchedulerSimulator/RoundRobin.cs
+++ SchedulerSimulator/RoundRobin.cs
@@ -15,9 +15,58 @@ namespace SchedulerSimulator
 
         private readonly Queue<ProcessControlBlock> readyQueue = new Queue<ProcessControlBlock>();
 
-        protected override bool Busy => throw new NotImplementedException();
+        protected override bool Busy => readyQueue.Any();
 
-        public override void Push(Process process) => throw new NotImplementedException();
-        protected override void Dispatch() => throw new NotImplementedException();
+        public override void Push(Process process)
+        {
+            if (
+                readyQueue.Any()
+                && process.ArrivalTime > readyQueue.First().Process.ArrivalTime
+            )
+            {
+                while (readyQueue.Any() && process.ArrivalTime > currentTime)
+                {
+                    Dispatch();
+                }
+            }
+            readyQueue.Enqueue(new ProcessControlBlock
+            {
+                Process = process,
+            });
+        }
+
+        protected override void Dispatch()
+        {
+            var before = readyQueue.Dequeue();
+            var pcb = (ProcessControlBlock)before.Clone();
+            var process = pcb.Process;
+
+            // 프로세스 도착 시간까지 현재 시간을 진행
+            if (currentTime < process.ArrivalTime)
+            {
+                currentTime = process.ArrivalTime;
+            }
+
+            if (pcb.ResponseTime == null)
+            {
+                pcb.ResponseTime = currentTime - process.ArrivalTime;
+            }
+            if (pcb.WaitingTime == null)
+            {
+                pcb.WaitingTime = currentTime - process.ArrivalTime;
+            }
+            else
+            {
+                // TODO
+                pcb.WaitingTime = before.WaitingTime;
+            }
+
+            currentTime += process.BurstTime;
+            pcb.BurstTime = process.BurstTime;
+
+            pcb.TurnaroundTime = currentTime - process.ArrivalTime;
+
+            OnProcessChanged(pcb);
+        }
     }
 }
